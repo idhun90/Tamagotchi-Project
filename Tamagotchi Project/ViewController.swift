26af8f3bcd6572e08a1cc814955c635635d09f36
idhun90 <idhun90@naver.com>
2022-07-22 15:30:18 +0900

    Initial Commit

---
+++ Tamagotchi Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tamagotchi Project
+//
+//  Created by 이도헌 on 2022/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
