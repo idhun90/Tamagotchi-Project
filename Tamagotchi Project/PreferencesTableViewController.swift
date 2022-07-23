d5a08ba57a3e0dfae1ef344cb9e332fe5f26d6a9
idhun90 <idhun90@naver.com>
2022-07-23 15:11:35 +0900

    초기화 버튼 클릭시 alret 추가.

--- Tamagotchi Project/PreferencesTableViewController.swift
+++ Tamagotchi Project/PreferencesTableViewController.swift
@@ -60,7 +60,29 @@ class PreferencesTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 60
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // '내 이름 설정하기' 항목 선택 시
+        // '다마고치 변경하기' 항목 선택 시
+        // '데이터 초기화' 항목 선택 시
+        if indexPath.row == 2 {
+            // 알람 추가
+            let alert = UIAlertController(title: "데이터 초기화", message: "모든 데이터가 삭제됩니다.", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "확인", style: .destructive) { _ in
+                // 데이터 초기화 구문 작성
+            }
+            let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+            alert.addAction(ok)
+            alert.addAction(cancel)
+            
+            present(alert, animated: true, completion: nil)
+        }
+        
+    }
 }
+
+
 
 // 사용자가 처음 앱을 실행했을 때, 앱 내부에서 미리 설정된 이름 중 랜덤으로 선택해서 '내 이름' 설정해주기
 // 사용자가 '내 이름'을 변경했을 때, 사용자가 변경한 이름으로 보여주도록 설정하기
